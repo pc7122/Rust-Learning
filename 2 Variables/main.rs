25f56b9b6db73b543001fa994327328996eaa81d
prathamesh chaudhary <prathameshchaudhary7122@gmail.com>
2024-08-04 16:56:23 +0530

    variables

---
+++ 2 Variables/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let a:i8 = -128;
+    let b:u8 = 128;
+    let c:&str = "Prathamesh";
+
+    println!("Signed integer: {}", a);
+    println!("Unsigned integer: {}", b);
+    println!("String: {}", c);
+}
